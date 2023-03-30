789e637cff8303821c879b77b9e769e707fe16dd
BramuelMwenda <98076661+BramuelMwenda@users.noreply.github.com>
2023-03-30 13:52:41 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var laptoppic=document.querySelector(".img-container");
+let laptopcont=document.querySelector(".laptop");
+
+laptopcont.addEventListener('click',()=>{
+
+
+        console.log("HI");
+        laptoppic.innerHTML="<div class='img-container'><img src='BimTech (2).PNG' </div>";
+        laptoppic.innerHTML="<div class='img-container'><img src='website template (2).png' </div>";
+        laptoppic.innerHTML="<div class='img-container'><img src='website template (3).png' </div>";
+        laptoppic.innerHTML="<div class='img-container'><img src='website template (4).png'</div>";
+        
+});
